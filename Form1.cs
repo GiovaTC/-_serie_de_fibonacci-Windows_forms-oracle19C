79bec1559095e679af12dfeb72237840a81d6ae4
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-25 10:59:20 -0500

    commit - script_serie_fibonacci :.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client; // NuGet: Oracle.ManagedDataAccess
+
+
+namespace FibonacciApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
